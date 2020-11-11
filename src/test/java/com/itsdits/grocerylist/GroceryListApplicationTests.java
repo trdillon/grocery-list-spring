f3f722b26f62edabe19cfdc82cca634bfd03598a
Tim Dillon <tim@itsdits.com>
2020-11-11 16:55:45 +0900

    Initial commit

---
+++ src/test/java/com/itsdits/grocerylist/GroceryListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itsdits.grocerylist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroceryListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
