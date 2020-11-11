fcf9dbc37d2c73ef6cb0579faf3c39867681b1d3
Tim Dillon <tim@itsdits.com>
2020-11-11 20:29:28 +0900

    Wrote GroceryService and expanded GroceryRepository

--- src/main/java/com/itsdits/grocerylist/repository/GroceryRepository.java
+++ src/main/java/com/itsdits/grocerylist/repository/GroceryRepository.java
@@ -3,5 +3,11 @@ package com.itsdits.grocerylist.repository;
 import com.itsdits.grocerylist.model.Grocery;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface GroceryRepository extends JpaRepository<Grocery, Long> {
+
+    List<Grocery> findByPurchased(boolean purchased);
+    List<Grocery> findByName(String name);
+
 }
---
+++ src/main/java/com/itsdits/grocerylist/service/GroceryService.java
@@ -0,0 +1,49 @@
+package com.itsdits.grocerylist.service;
+
+import com.itsdits.grocerylist.model.Grocery;
+import com.itsdits.grocerylist.repository.GroceryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class GroceryService {
+
+    @Autowired
+    private GroceryRepository repo;
+
+    public List<Grocery> getAll() {
+        return repo.findAll();
+    }
+
+    public Grocery get(long id) {
+        return repo.findById(id).orElse(null);
+    }
+
+    public List<Grocery> getNotPurchased() {
+        return repo.findByPurchased(false);
+    }
+
+    public List<Grocery> getPurchased() {
+        return repo.findByPurchased(true);
+    }
+
+    public List<Grocery> getByName(String name) {
+        return repo.findByName(name);
+    }
+
+    public void save(Grocery grocery) {
+        repo.save(grocery);
+    }
+
+    public void delete(long id) {
+        repo.deleteById(id);
+    }
+
+    public void purge() {
+        repo.deleteAll();
+    }
+}
