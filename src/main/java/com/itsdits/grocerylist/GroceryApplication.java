6715edfe5fe5c7a77d8d7ea737f809290f1ad519
Tim Dillon <tim@itsdits.com>
2020-11-11 20:33:13 +0900

    Created GroceryController skeleton

--- src/main/java/com/itsdits/grocerylist/GroceryApplication.java
+++ src/main/java/com/itsdits/grocerylist/GroceryApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GroceryListApplication {
+public class GroceryApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GroceryListApplication.class, args);
+        SpringApplication.run(GroceryApplication.class, args);
     }
 
 }
---
+++ src/main/java/com/itsdits/grocerylist/controller/GroceryController.java
@@ -0,0 +1,15 @@
+package com.itsdits.grocerylist.controller;
+
+import com.itsdits.grocerylist.service.GroceryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class GroceryController {
+
+    @Autowired
+    private GroceryService service;
+
+    //TODO - add handler methods
+
+}
