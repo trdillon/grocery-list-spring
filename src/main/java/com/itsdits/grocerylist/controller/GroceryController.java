87fdebbc06a42898bb83506d2565f8bd299053a7
Tim Dillon <tim@itsdits.com>
2020-11-12 12:31:56 +0900

    Added additional get methods to GroceryController

--- src/main/java/com/itsdits/grocerylist/controller/GroceryController.java
+++ src/main/java/com/itsdits/grocerylist/controller/GroceryController.java
@@ -6,11 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class GroceryController {
@@ -39,6 +41,46 @@ public class GroceryController {
         }
         catch (Exception e) {
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/grocery/{id}")
+    public ResponseEntity<Grocery> getGroceryById(@PathVariable("id") long id) {
+        Optional<Grocery> grocery = Optional.ofNullable(service.get(id));
+
+        return grocery.map(value -> new ResponseEntity<>(value, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @GetMapping("grocery/need")
+    public ResponseEntity<List<Grocery>> getNotPurchased() {
+        try {
+            List<Grocery> groceries = service.getNotPurchased();
+
+            if (groceries.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            } else {
+                return new ResponseEntity<>(groceries, HttpStatus.OK);
+            }
+        }
+        catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.EXPECTATION_FAILED);
+        }
+    }
+
+    @GetMapping("grocery/purchased")
+    public ResponseEntity<List<Grocery>> getPurchased() {
+        try {
+            List<Grocery> groceries = service.getPurchased();
+
+            if (groceries.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            } else {
+                return new ResponseEntity<>(groceries, HttpStatus.OK);
+            }
+        }
+        catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.EXPECTATION_FAILED);
         }
     }
 }
