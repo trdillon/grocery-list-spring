50605ab0ad5cf83455d77529881f394dd55c3032
Tim Dillon <tim@itsdits.com>
2020-11-17 11:44:49 +0900

    Added CORS config

--- src/main/java/com/itsdits/grocerylist/security/config/WebSecurityConfig.java
+++ src/main/java/com/itsdits/grocerylist/security/config/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.itsdits.grocerylist.security.config;
 
+import com.google.common.collect.ImmutableList;
 import com.itsdits.grocerylist.security.jwt.AuthEntryPointJwt;
 import com.itsdits.grocerylist.security.jwt.AuthTokenFilter;
 import com.itsdits.grocerylist.security.service.UserDetailsServiceImpl;
@@ -16,6 +17,9 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -59,9 +63,27 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeRequests()
                 .antMatchers("/api/auth/**").permitAll()
-                .antMatchers("/api/grocery/**").access("hasRole('USER') or hasRole('ADMIN')")
+                .antMatchers("/api/grocery/**").permitAll() // For public use with no auth
+                //.antMatchers("/api/grocery/**").access("hasRole('USER') or hasRole('ADMIN')") // For use when auth is impl
                 .anyRequest().authenticated();
 
         http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        final CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.setAllowedOrigins(ImmutableList.of("https://www.yourdomain.com")); // Production domain
+        // configuration.setAllowedOrigins(ImmutableList.of("http://localhost:3000") // React dev env
+        // configuration.setAllowedOrigins(ImmutableList.of("*"));  // All domains
+        configuration.setAllowedMethods(ImmutableList.of("GET", "POST", "PUT", "DELETE"));
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedHeaders(ImmutableList.of("Authorization", "Cache-Control", "Content-Type"));
+
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
     }
 }
