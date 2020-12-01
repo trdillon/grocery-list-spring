342bb50998b0a66b625dcf1cbdd96baf8d75e44a
Tim Dillon <tim@itsdits.com>
2020-12-01 15:40:58 +0900

    Removed javadocs because they were unnecessary and cluttered the code. Will add comments where needed as the classes are refactored.

--- src/main/java/com/itsdits/grocerylist/controller/GroceryController.java
+++ src/main/java/com/itsdits/grocerylist/controller/GroceryController.java
@@ -13,13 +13,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-/**
- * GroceryController.java - This class provides a REST controller for the grocery API endpoints.
- * Utilizes an instance of {@link GroceryService} to handle the logic of the API calls.
- *
- * @author Tim Dillon
- * @version 1.1
- */
 @RestController
 @RequestMapping("/api")
 public class GroceryController {
@@ -31,13 +24,6 @@ public class GroceryController {
         this.service = service;
     }
 
-    /**
-     * GET call gets a list of all Grocery items, or any matching the search criteria if provided.
-     *
-     * @param name Optional String param for searching
-     * @return List of all Grocery objects, List of Grocery objects containing name param, or
-     *         HttpStatus.NO_CONTENT if List is empty
-     */
     @GetMapping("/grocery")
     public ResponseEntity<List<Grocery>> getGroceryList(@RequestParam(required = false) String name) {
         List<Grocery> groceries = new ArrayList<>();
@@ -55,13 +41,6 @@ public class GroceryController {
         return new ResponseEntity<>(groceries, HttpStatus.OK);
     }
 
-    /**
-     * GET call gets a single Grocery item by its id value.
-     *
-     * @param id long param representing the id of the desired Grocery item
-     * @return Grocery object with matching id
-     * @throws ResourceNotFoundException if no matching id was found
-     */
     @GetMapping("/grocery/{id}")
     public ResponseEntity<Grocery> getGroceryById(@PathVariable("id") long id) {
         Optional<Grocery> grocery = Optional.ofNullable(service.getById(id));
@@ -70,12 +49,6 @@ public class GroceryController {
                 .orElseThrow(() -> new ResourceNotFoundException("No grocery with the ID " + id + " was found."));
     }
 
-    /**
-     * GET call gets a List of Grocery items that need to be purchased.
-     *
-     * @return List of Grocery objects where {@code purchased = false} or
-     *         HttpStatus.NO_CONTENT if no items match criteria
-     */
     @GetMapping("/grocery/need")
     public ResponseEntity<List<Grocery>> getNotPurchased(@RequestParam(required = false) String name) {
         List<Grocery> groceries = new ArrayList<>();
@@ -93,12 +66,6 @@ public class GroceryController {
         return new ResponseEntity<>(groceries, HttpStatus.OK);
     }
 
-    /**
-     * GET call gets a List of Grocery items that have already been purchased.
-     *
-     * @return List of Grocery objects where {@code purchased = true} or
-     *         HttpStatus.NO_CONTENT if no items match criteria
-     */
     @GetMapping("/grocery/purchased")
     public ResponseEntity<List<Grocery>> getPurchased(@RequestParam(required = false) String name) {
         List<Grocery> groceries = new ArrayList<>();
@@ -116,12 +83,6 @@ public class GroceryController {
         return new ResponseEntity<>(groceries, HttpStatus.OK);
     }
 
-    /**
-     * GET call gets a List of Grocery items the user marked as favorites.
-     *
-     * @return List of Grocery objects where {@code favorite = true} or
-     *         HttpStatus.NO_CONTENT if no items match criteria
-     */
     @GetMapping("/grocery/favs")
     public ResponseEntity<List<Grocery>> getFavs(@RequestParam(required = false) String name) {
         List<Grocery> groceries = new ArrayList<>();
@@ -139,12 +100,6 @@ public class GroceryController {
         return new ResponseEntity<>(groceries, HttpStatus.OK);
     }
 
-    /**
-     * POST call creates a new Grocery item.
-     *
-     * @param grocery Grocery object
-     * @return new Grocery object _grocery
-     */
     @PostMapping("/grocery")
     public ResponseEntity<Grocery> addGrocery(@RequestBody Grocery grocery) {
         Grocery _grocery = service.save(new Grocery(
@@ -153,14 +108,6 @@ public class GroceryController {
         return new ResponseEntity<>(_grocery, HttpStatus.CREATED);
     }
 
-    /**
-     * PUT call updates Grocery item based on id param.
-     *
-     * @param id long param representing the id of the desired Grocery item
-     * @param grocery Grocery object holding data to be saved to the existing Grocery item
-     * @return updated Grocery object _grocery
-     * @throws ResourceNotFoundException if no matching id was found
-     */
     @PutMapping("/grocery/{id}")
     public ResponseEntity<Grocery> editGrocery(@PathVariable("id") long id, @RequestBody Grocery grocery) {
         Optional<Grocery> groceryData = Optional.ofNullable(service.getById(id));
@@ -179,23 +126,12 @@ public class GroceryController {
         }
     }
 
-    /**
-     * DELETE call deletes a single Grocery item based on id param.
-     *
-     * @param id long param representing the id of the desired Grocery item
-     * @return HttpStatus.NO_CONTENT
-     */
     @DeleteMapping("/grocery/{id}")
     public ResponseEntity<HttpStatus> deleteGrocery(@PathVariable("id") long id) {
         service.delete(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
-    /**
-     * DELETE call deletes all Grocery items in the database.
-     *
-     * @return HttpStatus.NO_CONTENT
-     */
     @DeleteMapping("/grocery")
     public ResponseEntity<HttpStatus> deleteGroceryList() {
         service.purge();
