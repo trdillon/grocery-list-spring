b35c7028c9658519e814669a90e204803310aae2
Tim Dillon <tim@itsdits.com>
2020-12-01 15:51:45 +0900

    Finished User entity

--- src/main/java/com/itsdits/grocerylist/model/User.java
+++ src/main/java/com/itsdits/grocerylist/model/User.java
@@ -1,8 +1,15 @@
 package com.itsdits.grocerylist.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class User {
 
@@ -10,13 +17,4 @@ public class User {
     private String id;
     private String name;
     private String email;
-
-    public User() {
-    }
-
-    public User(String id, String name, String email) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-    }
 }
