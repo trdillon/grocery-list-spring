e485ad62e30039f850ab7fcb1a1ef88e9a904237
Tim Dillon <tim@itsdits.com>
2020-12-03 20:01:04 +0900

    Refactored GroceryController to authorize methods by authority

--- src/main/java/com/itsdits/grocerylist/controller/GroceryController.java
+++ src/main/java/com/itsdits/grocerylist/controller/GroceryController.java
@@ -2,14 +2,11 @@ package com.itsdits.grocerylist.controller;
 
 import com.itsdits.grocerylist.exception.ResourceNotFoundException;
 import com.itsdits.grocerylist.model.Grocery;
-import com.itsdits.grocerylist.model.User;
-import com.itsdits.grocerylist.repository.UserRepository;
 import com.itsdits.grocerylist.service.GroceryService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -17,7 +14,6 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.security.Principal;
 import java.util.Collection;
-import java.util.Map;
 import java.util.Optional;
 
 @RestController
@@ -26,11 +22,9 @@ public class GroceryController {
 
     private final Logger log = LoggerFactory.getLogger(GroceryController.class);
     private final GroceryService groceryService;
-    private final UserRepository userRepository;
 
-    public GroceryController(GroceryService groceryService, UserRepository userRepository) {
+    public GroceryController(GroceryService groceryService) {
         this.groceryService = groceryService;
-        this.userRepository = userRepository;
     }
 
     @GetMapping("/groceries")
@@ -39,6 +33,7 @@ public class GroceryController {
     }
 
     @GetMapping("/grocery")
+    @PreAuthorize("hasAuthority('SCOPE_profile')")
     Collection<Grocery> groceryList(Principal principal) {
         return groceryService.getByUserId(principal.getName());
     }
@@ -51,22 +46,15 @@ public class GroceryController {
     }
 
     @PostMapping("/grocery")
-    ResponseEntity<Grocery> createGrocery(@Valid @RequestBody Grocery grocery,
-                                          @AuthenticationPrincipal OAuth2User principal) throws URISyntaxException {
+    @PreAuthorize("hasAuthority('SCOPE_profile')")
+    ResponseEntity<Grocery> createGrocery(@Valid @RequestBody Grocery grocery) throws URISyntaxException {
         log.info("Request to create grocery: {}", grocery);
-        Map<String, Object> data = principal.getAttributes();
-        String userId = data.get("sub").toString();
-
-        // check to see if user already exists
-        Optional<User> user = userRepository.findById(userId);
-        grocery.setUser(user.orElse(new User(userId,
-                data.get("name").toString(), data.get("email").toString())));
-
         Grocery result = groceryService.save(grocery);
         return ResponseEntity.created(new URI("/api/grocery/" + result.getId())).body(result);
     }
 
     @PutMapping("/grocery/{id}")
+    @PreAuthorize("hasAuthority('SCOPE_profile')")
     ResponseEntity<Grocery> updateGrocery(@Valid @RequestBody Grocery grocery) {
         log.info("Request to update grocery: {}", grocery);
         Grocery result = groceryService.save(grocery);
@@ -74,6 +62,7 @@ public class GroceryController {
     }
 
     @DeleteMapping("/grocery/{id}")
+    @PreAuthorize("hasAuthority('SCOPE_profile')")
     public ResponseEntity<?> deleteGrocery(@PathVariable Long id) {
         log.info("Request to delete grocery: {}", id);
         groceryService.delete(id);
