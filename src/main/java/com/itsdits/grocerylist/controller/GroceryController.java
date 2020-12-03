04aed2f2e50ea05892ade94ae18ad88dadad3520
Tim Dillon <tim@itsdits.com>
2020-12-03 13:41:08 +0900

    Added user data to GroceryController

--- src/main/java/com/itsdits/grocerylist/controller/GroceryController.java
+++ src/main/java/com/itsdits/grocerylist/controller/GroceryController.java
@@ -2,11 +2,14 @@ package com.itsdits.grocerylist.controller;
 
 import com.itsdits.grocerylist.exception.ResourceNotFoundException;
 import com.itsdits.grocerylist.model.Grocery;
+import com.itsdits.grocerylist.model.User;
 import com.itsdits.grocerylist.repository.UserRepository;
 import com.itsdits.grocerylist.service.GroceryService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -14,6 +17,7 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.security.Principal;
 import java.util.Collection;
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
@@ -47,8 +51,17 @@ public class GroceryController {
     }
 
     @PostMapping("/grocery")
-    ResponseEntity<Grocery> createGrocery(@Valid @RequestBody Grocery grocery) throws URISyntaxException {
+    ResponseEntity<Grocery> createGrocery(@Valid @RequestBody Grocery grocery,
+                                          @AuthenticationPrincipal OAuth2User principal) throws URISyntaxException {
         log.info("Request to create grocery: {}", grocery);
+        Map<String, Object> data = principal.getAttributes();
+        String userId = data.get("sub").toString();
+
+        // check to see if user already exists
+        Optional<User> user = userRepository.findById(userId);
+        grocery.setUser(user.orElse(new User(userId,
+                data.get("name").toString(), data.get("email").toString())));
+
         Grocery result = groceryService.save(grocery);
         return ResponseEntity.created(new URI("/api/grocery/" + result.getId())).body(result);
     }
