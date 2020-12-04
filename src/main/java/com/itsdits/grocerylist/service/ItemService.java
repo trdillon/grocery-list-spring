d368ccb926b0f52ca3b08780de350523c2326d69
Tim Dillon <tim@itsdits.com>
2020-12-04 10:53:14 +0900

    Created ItemService

---
+++ src/main/java/com/itsdits/grocerylist/service/ItemService.java
@@ -0,0 +1,40 @@
+package com.itsdits.grocerylist.service;
+
+import com.itsdits.grocerylist.model.Item;
+import com.itsdits.grocerylist.model.User;
+import com.itsdits.grocerylist.repository.ItemRepository;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+@Transactional
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    public ItemService(ItemRepository itemRepository) {
+        this.itemRepository = itemRepository;
+    }
+
+    public List<Item> getUserGroceryList(User user) {
+        return itemRepository.findAllByUserId(user.getId());
+    }
+
+    public List<Item> getByProductName(String name) {
+        return itemRepository.findByProductContaining(name);
+    }
+
+    public List<Item> getByNeeded() {
+        return itemRepository.findByPurchased(false);
+    }
+
+    public List<Item> getByPurchased() {
+        return itemRepository.findByPurchased(true);
+    }
+
+    public List<Item> getByFavorite() {
+        return itemRepository.findByFavorite(true);
+    }
+}
