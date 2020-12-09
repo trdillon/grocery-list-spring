5f3d074c50f304e251b3951e630eed50d2c1b988
Tim Dillon <tim@itsdits.com>
2020-12-09 16:20:53 +0900

    Changed SecurityConfig to allow unauthenticated access to /groceries path

--- src/main/java/com/itsdits/grocerylist/config/SecurityConfig.java
+++ src/main/java/com/itsdits/grocerylist/config/SecurityConfig.java
@@ -4,13 +4,21 @@ import com.okta.spring.boot.oauth.Okta;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web) {
+        web
+                .ignoring()
+                .antMatchers("/api/groceries");
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
